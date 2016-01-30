26a5ee1489e8bf0e12fd08ad530f1c35f65dd6a8
shubhadaPatil <shubhada.d.patil@gmail.com>
2016-01-30 16:45:45 -0500

    first push
    first push

---
+++ jenkinDemoclass.java
@@ -0,0 +1,13 @@
+package demoJenkinpkg;
+
+import org.testng.annotations.Test;
+
+public class jenkinDemoclass {
+
+	@Test
+	public void testJenkins()
+	{
+		System.out.println("Welcome Nitin in the world of Jenkins");
+		
+	}
+}
