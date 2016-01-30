1abed7d9401cc796d93f054bc33ca1309e9728db
shubhadaPatil <shubhada.d.patil@gmail.com>
2016-01-30 17:53:40 -0500

    Revert "hh"
    This reverts commit fb87fa6880de911c4d86d49f5ef0d7c8f9cdc3ba.
    
    # Conflicts:
    #	jenkinDemoclass.java

--- jenkinDemoclass.java
+++ jenkinDemoclass.java
@@ -8,7 +8,11 @@ public class jenkinDemoclass {
 	public void testJenkins()
 	{
 		System.out.println("Welcome Nitin in the world of Jenkins");
+<<<<<<< HEAD
 		System.out.println("test pull-Shubhada Patil-Shirsat");
+=======
+		System.out.println("test pull");
+>>>>>>> parent of fb87fa6... hh
 		
 	}
 }
